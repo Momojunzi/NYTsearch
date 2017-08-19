8b0b60ff30928384cd159af600cef1f5d741c0cc
Jonathan Tung <jtung23@gmail.com>
2017-08-19 14:22:07 -0700

    some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-var term = $('#search_term');
-var numberRecord = $('#num_rec');
+var term = $('#search_term').html();
+var numberRecord = $('#num_rec').html();
 var startYear = $('#start_year');
 var endYear = $('#end_year');
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
-  "api_key=f0f27ef90a834d6989d3ed94b6714223" + "&q=" + term;
-// numberREcord + startYear + endYear + 
-
+  "api_key=f0f27ef90a834d6989d3ed94b6714223" + "&q=" + term + "&fl=" + numberRecord; 
 
 $('#search').on("click", function() {
+	console.log(term)
+	console.log(queryURL)
 	$.ajax({
   url: queryURL,
   method: "GET"
