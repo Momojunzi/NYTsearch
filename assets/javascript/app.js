99a6b98d15bd9cee5a3384dc02aa8a6deb71ab3d
shirinR <shirin.raad@gmail.com>
2017-08-20 20:21:54 -0700

    update app.js + add some styling to the added articles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   $('#search').on("click", function() {
 
+    var articleCounter = 1;
     var term = $('#search_term').val();
     var numberRecord = $('#num_rec').val();
     var startYear = $('#start_year').val();
@@ -11,22 +12,27 @@ $(document).ready(function(){
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" +
       "?&api_key=" + APIKey + "&?q=" + term + "&?fl=" + numberRecord + "&?begin_date=" + startYear + "&?end_date=" + endYear;
 
-      console.log('URL>>>', queryURL);
-
   	$.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
-      console.log(response);
 
       var results = response.response.docs;
       console.log(results);
 
+      for(var i=0; i<results.length; i++){
+        var resultTag = $('<div>');
+        resultTag.addClass('wellSection');
+        resultTag.append('<h4><span class="label label-primary"><strong>'+ (articleCounter++) +'. ' + results[i].snippet +'</span></h3>');
+        resultTag.append('<h5>Section:' + results[i].section_name+'</h5>');
+        resultTag.append('<h5>' + results[i].pub_date+'</h5>');
+        resultTag.append('<a href="' + results[i].web_url+'">' + results[i].web_url +'</a><br><br>');
+        $('#results_container').append(resultTag);
+      }
     });
   });
 
   $('#submitbtn').on("click", function() {
-  	console.log('clear button');
     $('#search_term').empty();
     $('#num_rec').empty();
     $('#start_year').empty();
