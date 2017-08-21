2f688794ebf51b3ec602537a82aaef8882622cb6
shirinR <shirin.raad@gmail.com>
2017-08-20 19:47:10 -0700

    edit app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,37 @@
 $(document).ready(function(){
-var term = $('#search_term').html();
-var numberRecord = $('#num_rec').html();
-var startYear = $('#start_year');
-var endYear = $('#end_year');
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
-  "api_key=f0f27ef90a834d6989d3ed94b6714223" + "&q=" + term + "&fl=" + numberRecord; 
+  $('#search').on("click", function() {
 
-$('#search').on("click", function() {
-	console.log(term)
-	console.log(queryURL)
-	$.ajax({
-  url: queryURL,
-  method: "GET"
-}).done(function(response) {
-  console.log(response);
-  console.log('yes')
+    var term = $('#search_term').val();
+    var numberRecord = $('#num_rec').val();
+    var startYear = $('#start_year').val();
+    var endYear = $('#end_year').val();
+    var APIKey = "f0f27ef90a834d6989d3ed94b6714223";
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" +
+      "?&api_key=" + APIKey + "&?q=" + term + "&?fl=" + numberRecord + "&?begin_date=" + startYear + "&?end_date=" + endYear;
+
+      console.log('URL>>>', queryURL);
+
+  	$.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+
+      var results = response.response.docs;
+      console.log(results);
+
+    });
   });
 
-$('#submitbtn').on("click", function() {
-	console.log('no')
-})
+  $('#submitbtn').on("click", function() {
+  	console.log('clear button');
+    $('#search_term').empty();
+    $('#num_rec').empty();
+    $('#start_year').empty();
+    $('#end_year').empty();
+  });
 
-})
 
-})
-
+});
